7ff465150ba6befa34f599b7f6f296fd8c41cf1c
Tony Baker <this.tonybaker@gmail.com>
2019-05-24 03:18:47 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,7 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -54,4 +54,3 @@ class InteractiveRecord
   end
 
 end
-
